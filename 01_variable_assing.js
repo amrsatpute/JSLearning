b30c4bf5c3a8bb3249af06881536f2906099b69c
amrsatpute <amardev862@gmail.com>
2023-02-16 12:03:00 +0530

    Variable declaration
    01.Variable declaration

---
+++ 01_variable_assing.js
@@ -0,0 +1,59 @@
+//undefined city name//
+console.log("*******Step 1. undefined city name ******");
+var city;
+console.log("City Name:", city);
+//fullName//
+console.log("*******Step 2. fullName ******");
+var myFullName = "Amar Baliram Satpute";
+console.log("Full Name:", myFullName);
+//FirstNAme lastName//
+console.log("*******Step 3. firstName lastName ******");
+var firstName = "Amar";
+console.log("My First Name:", firstName);
+//last Name//
+var lastName = "Satpute";
+console.log("My Last Name:", lastName);
+//***Love Javascript **/
+console.log("*******Step 4.Love Javascript ******");
+var myLove = "I love Js";
+console.log("My Love Name:", myLove);
+//age define//
+console.log("*******Step 5.age define ******");
+var age = "27"; //variable decalartion and initialization in same line
+var age = "28";
+console.log("My Age:", age);
+//address//
+console.log("*******Step 6.address define ******");
+var address = "Kothrud";
+console.log("My Village Name", address);
+//Pincode//
+console.log("*******Step 7.pin code define ******");
+var pinCode = "411038";
+console.log("area Pin code", pinCode);
+//Gender//
+console.log("*******Step 8.gender define ******");
+var gender = "male";
+console.log("Gender:", gender);
+//isMarrid//
+console.log("*******Step 9.Marreid Status define ******");
+var isMarried = "Yes";
+console.log("Married Status", isMarried);
+console.log("*******Step 10.All details one lane Define ******");
+var firstName = "Amar";
+var lastname = "Satpute";
+console.log(
+  "FirstName:",
+  firstName,
+  "LastName",
+  lastName,
+  "age",
+  age,
+  "Gender",
+  gender,
+  "Married",
+  isMarried,
+  "Address",
+  address,
+  "Pincode",
+  pinCode
+);
