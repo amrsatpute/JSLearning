ecf333c5cc2fae30506b9c560755c4c64fc33cd6
amrsatpute <amardev862@gmail.com>
2023-02-16 12:02:08 +0530

    Print on name
    Name Changes

--- helloWorld.js
+++ helloWorld.js
@@ -1,1 +1,9 @@
 console.log("Amar satpute");
+console.log("27");
+console.log("Pune");
+
+var fullName = "Amar Satpute";
+console.log(fullName);
+console.log(fullName);
+var city;
+console.log(city);
