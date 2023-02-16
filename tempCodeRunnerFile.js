e578a864162d3de8f004e65aaa57e0490ca1ee72
amrsatpute <amardev862@gmail.com>
2023-02-16 13:54:33 +0530

    Create tempCodeRunnerFile.js
    temp Code file variable

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,35 @@
+//undefined city name//
+var city;
+console.log(city);
+//fisrtname//
+var myFullName = "Amar Baliram Satpute";
+console.log(myFullName);
+//FirstNAme lastName//
+var firstName = "Amar";
+ console.log(firstName);
+//last Name//
+var lastName = "Satpute";
+ console.log(lastName);
+ //***Love Javascript **/
+var myLove = "I love Js";
+console.log(myLove);
+//age define//
+var age = "27";
+console.log(age);
+//address//
+var address = "Kothrud";
+console.log(address);
+//Pincode//
+var pinCode = "411038";
+console.log(pinCode);
+//Gender//
+var gender = "male";
+console.log(address);
+//isMarrid//
+var isMarried = "Yes";
+console.log(isMarried);
+
+var firstName = "Amar"
+var lastname = "Satpute"
+
+console.log(firstName,lastname,age,address,pinCode);
