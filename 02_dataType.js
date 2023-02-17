da2210c5123e2bb51c23a2a485c04b8be46d4ffa
amrsatpute <amardev862@gmail.com>
2023-02-17 14:05:35 +0530

    data type

---
+++ 02_dataType.js
@@ -0,0 +1,25 @@
+ var myName = 'Virat Kohli';
+ var fatherName = "Yogesh Kohli"
+
+ var age = 31;
+ var weight = 82.5;
+
+ var isMarried = true;
+ var isLazy = false; 
+ var graduation;
+ var typeOfMyName  = typeof myName;
+ console.log("Value of  graduation variable is: ", graduation);
+ console.log("typeOf graduation is: ", graduation);
+
+ console.log("Typeof variable myName: ",  typeOfMyName);
+ console.log("typeOf variable fatherName:", typeoffatherName);
+ console.log("typeOf variable age", typeofage);
+
+// console.log("Swapping variables");
+//  var num1 = 100; // 200
+// var num2 = 200; // 100
+
+//  var temp = num1;
+//  num1 = num2; 
+//  num2 = temp;
+//  console.log(num1, num2);
