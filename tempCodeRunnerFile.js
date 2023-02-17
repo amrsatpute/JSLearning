12660c5f2b0dc5d8b3d0f3c6d1516f9601255e5a
amrsatpute <amardev862@gmail.com>
2023-02-17 14:06:38 +0530

    add new data types
    data types

--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,35 +1,16 @@
-//undefined city name//
-var city;
-console.log(city);
-//fisrtname//
-var myFullName = "Amar Baliram Satpute";
-console.log(myFullName);
-//FirstNAme lastName//
-var firstName = "Amar";
- console.log(firstName);
-//last Name//
-var lastName = "Satpute";
- console.log(lastName);
- //***Love Javascript **/
-var myLove = "I love Js";
-console.log(myLove);
-//age define//
-var age = "27";
-console.log(age);
-//address//
-var address = "Kothrud";
-console.log(address);
-//Pincode//
-var pinCode = "411038";
-console.log(pinCode);
-//Gender//
-var gender = "male";
-console.log(address);
-//isMarrid//
-var isMarried = "Yes";
-console.log(isMarried);
+ var myName = 'Virat Kohli';
+ var fatherName = "Yogesh Kohli"
 
-var firstName = "Amar"
-var lastname = "Satpute"
+ var age = 31;
+ var weight = 82.5;
 
-console.log(firstName,lastname,age,address,pinCode);
+ var isMarried = true;
+ var isLazy = false; 
+ var graduation;
+ var typeOfMyName  = typeof myName;
+ console.log("Value of  graduation variable is: ", graduation);
+ console.log("typeOf graduation is: ", graduation);
+
+ console.log("Typeof variable myName: ",  typeOfMyName);
+ console.log("typeOf variable fatherName:", typeoffatherName);
+ console.log("typeOf variable age", typeofage);
