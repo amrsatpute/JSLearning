4011981ebdd5722d84974acf1b6516afc2a6266a
amrsatpute <amardev862@gmail.com>
2023-02-17 14:04:43 +0530

    Basic variable
    Basic variable

---
+++ 01_variableBasicAssing.js
@@ -0,0 +1,77 @@
+// console.log("***assing.String,Number,Boolean.01**");
+var myName = "sky yadav";
+var fatherName = "prem yadav";
+
+var age = 34;
+var weight = 82.7;
+
+var isMarried = true;
+var islazy = false;
+
+var typeofmyName = typeof myName;
+console.log("Typeof variable myName:", typeofmyName);
+console.log("Typeof variable fatherName:", typeof fatherName);
+console.log("Typeof variable age:", typeof age);
+console.log("Typeof variable isMarried:", typeof isMarried);
+console.log("Typeof variable weight:", typeof weight);
+console.log("Typeof variable islazy:", typeof islazy);
+
+var graduation;
+console.log("value of variable graduation is:", typeof gradution);
+console.log("typeof graduation is:", typeof gradution);
+
+var city = null;
+console.log("Value of city variable is:", city);
+console.log("typeof city is:", typeof city);
+age = "32";
+console.log("typeof age", typeof age);
+
+console.log("***Number.01***");
+var pin = 412038;
+console.log("Typeof variable:", typeof pin);
+var college;
+console.log("Typeof variable college is:", typeof college);
+console.log("***String.02***");
+var bankName;
+bankName = "SBI Bank";
+console.log("bankName");
+console.log("Typeof variable is ", typeof bankName);
+console.log("typeof variable bankName is:", typeof bankName);
+
+console.log("assing. 02");
+var marks = "90%";
+console.log("typeof variable is:", typeof marks);
+var isWorking = true;
+console.log(isWorking);
+console.log("Typeof variable isWorking:", typeof isWorking);
+var totalCount = "Hundred and Seven";
+var totalCount = 107;
+var totalCount = 117;
+console.log("typeof variable is totalcount:", typeof totalcount, "My totalCount:",totalCount);
+
+console.log("Practicle.0 swappng variable");
+
+var sweety = "sweety";
+var cutie = "cutie";
+var temp = "sweety";
+sweety = cutie;
+cutie = temp;
+console.log(sweety, cutie);
+
+console.log("Assing:03 swappng variable");
+
+var num1 = 100; //200
+var num2 = 200; //100
+var temp = num1;
+num1 = num2;
+num2 = temp;
+console.log(num1, num2);
+
+console.log("Assing:03 swappng variable");
+
+var num2 = 200; //300
+var num3 = 300; //200
+var temp = num2;
+num2 = num3;
+num3 = temp;
+console.log(num2, num3);
