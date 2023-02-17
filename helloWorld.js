053394163e6cf97f3505aa64e8e2e614afa3fdac
amrsatpute <amardev862@gmail.com>
2023-02-17 14:05:59 +0530

    changes file delete

--- helloWorld.js
+++
@@ -1,9 +0,0 @@
-console.log("Amar satpute");
-console.log("27");
-console.log("Pune");
-
-var fullName = "Amar Satpute";
-console.log(fullName);
-console.log(fullName);
-var city;
-console.log(city);
