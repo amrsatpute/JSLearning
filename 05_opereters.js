87ca25b0946a6e95181e167d0d73e6a8dccc28cf
amrsatpute <amardev862@gmail.com>
2023-02-22 23:54:51 +0530

    operator

---
+++ 05_opereters.js
@@ -0,0 +1,63 @@
+console.log("****01****");
+function sqareOfWordLentgh(word) {
+  console.log(`word lenght is ${word.Length}`);
+  return word.Length ** 2;
+}
+console.log(`//Lenght of javascript//`);
+var wordlenght = sqareOfWordLentgh("Google Chrome");
+console.log(`Square of string javascript is ${wordlenght}`);
+console.log(`//Lenth of DEV smart//`);
+
+var worldDeveloperSmart = sqareOfWordLentgh("Developer Smart");
+
+console.log(`Squre of string developer smart is ${worldDeveloperSmart}`);
+
+console.log("****02****");
+
+function str() {
+  console.log(`The string is : I am angular Developer`);
+}
+str()
+
+var len=` I am angular Developer`
+var lenghtstr=len.length;
+console.log(`lenth of dtring = ${lenghtstr}`);
+var spl=len.split(" ")
+console.log(`Lenght of total words in string = $ {spl.lenght}`);
+var div=lenghtstr/spl.length;
+console.log(`Strin lenght divided by total number of words in string =${div}`);
+var multiplstr=lenghtstr*spl.length;
+console.log(`Multiplication of string lenght and total words available = ${multiplstr}`);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function stringBasics() {
+//   console.log("My dream company is: TCS");
+//   var hobby1 = "playing cricket,",
+//     hobby2 = "reading books,",
+//     hobby3 = "watching motivational vedios";
+
+//   console.log("My hobbys are:", hobby1, hobby2, hobby3);
+//   var hobby1Length = hobby1.length;
+//   console.log("length of hobby1:", hobby1Length);
+//   var hobby2Length = hobby2.length;
+//   console.log("length of hobby2:", hobby2Length);
+//   var hobby3Length = hobby3.length;
+//   console.log("length of hobby3:", hobby3Length);
+
+//   var sum = hobby1Length + hobby2Length + hobby3Length;
+//   console.log("Total number of character that is available in hobby=", sum);
+// }
+// stringBasics();
