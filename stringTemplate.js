e9024537db470547740a4b0187bcfc3ccaf81f07
amrsatpute <amardev862@gmail.com>
2023-02-22 23:56:25 +0530

    ES6 string template

---
+++ stringTemplate.js
@@ -0,0 +1,9 @@
+console.log(`You are "Chmap" man`);
+
+var firstName = "Amar";
+var lastName = "Satpute";
+var city = "pune";
+
+console.log("My First Name:",firstName, "lastName:", lastName, "city:", city);
+
+console.log(`FirstName: ${firstName} Lastname ${lastName} city ${city}`);
