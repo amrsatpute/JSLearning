887e3d80c6cab6a05dc2ef42d12830b537db8cad
amrsatpute <amardev862@gmail.com>
2023-02-24 13:28:37 +0530

    Update 01_functionAssing.js

--- 01_functionAssing.js
+++ 01_functionAssing.js
@@ -1,6 +1,30 @@
-function greaterNumber(num1, num2){
-    var result = num1>num2 ? num1 : num2;
-     console.log(`Greater number amongst ${num1}, ${num2} is: ${result} `);
- }
- greaterNumber(10, -10);
- greaterNumber(800, 899);
+console.log("---03 Assingment---");
+
+function maleMarriageEligibility(gender, age, boyName) {
+  var result =
+    gender == "Male" && age >= 21
+      ? `Hey ${boyName} you are eligible for marriage`
+      : `Hey ${boyName} you are not eligible for marriage`;
+  return result;
+}
+
+var result = maleMarriageEligibility("Male", 25, "Swapnill");
+console.log(result);
+var result1 = maleMarriageEligibility("Male", 17, "Akshay");
+console.log(result1);
+
+function femaleMarriageEligibility(gender, age, girlName) {
+  var result =
+    gender == "female" && age >= 18
+      ? `Hey ${girlName} you are eligible for marriage`
+      : `Hey ${girlName} you are not eligible for marriage`;
+  return result;
+}
+
+console.log("*****femaleMarriageEligibility*****");
+
+var result = femaleMarriageEligibility("female", 16, "Akshada");
+console.log(result);
+
+var result1 = femaleMarriageEligibility("female", 27, "Rashmika");
+console.log(result1);
