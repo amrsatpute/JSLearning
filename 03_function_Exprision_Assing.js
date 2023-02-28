9c694e80da4d443e5e8541b19487b534ceb10a4c
amrsatpute <amardev862@gmail.com>
2023-02-28 13:23:22 +0530

    function Expression
    function Expression

---
+++ 03_function_Exprision_Assing.js
@@ -0,0 +1,17 @@
+var leapYear = function checkeLeapYear(year) {
+  if (year % 4 == 0) {
+    console.log(`The Given year "${year}" is : Leap Year`);
+  }
+  if (year % 4!= 0 && (typeof year=="number")) {
+    console.log(`The Given year "${year}" is : not Leap Year`);
+  }
+  if (year==undefined|| year==NaN||year=="twenty twenty") {
+    console.log(`The Given year "${year}" is not valid ,please provide valid data`);
+  }
+};
+leapYear(2020);
+leapYear(1999);
+leapYear(1600);
+leapYear(2022);
+leapYear(1945);
+leapYear(null);
