1f3fc5f73aa0c17391605d5d0366b34f3b70b838
amrsatpute <amardev862@gmail.com>
2023-03-04 22:35:55 +0530

    06 Factorial assingment
    06 Factorial assignment

---
+++ 06_factorial_Assing_B.js
@@ -0,0 +1,27 @@
+function factorialOfWordsCount(string)
+{
+ 
+ if(string==null||string==undefined || (isNaN(string) && typeof string=="number"))
+ {
+   console.log(`Given string ${string} is invalid, please entre valid data`);
+   return;
+ }
+ else
+ {
+   
+   let fact = 1;
+   let word = string.split(" ");
+   let length = word.length;
+    for (let i = 1; i <= word.length; i++) 
+    {
+        fact =fact * i;
+    }
+    console.log(`Given string is ${string}`);
+    console.log(`Word count of given string is ${word.length} . The factorial of words count is ${fact}.`);
+  }
+}
+factorialOfWordsCount("Revision is the mother of sucess");
+factorialOfWordsCount(null);
+factorialOfWordsCount("");
+factorialOfWordsCount("Amar Baliram Satpute");
+factorialOfWordsCount("We never fail, we always learn");
