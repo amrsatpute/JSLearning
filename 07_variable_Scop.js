4e638775d946f134456ea017b6f456287afdb41d
amrsatpute <amardev862@gmail.com>
2023-03-06 17:32:20 +0530

    variables scop
    scop

---
+++ 07_variable_Scop.js
@@ -0,0 +1,10 @@
+var firstName; //Variable declaration
+firstName = "amar"; //Variable Initialization
+
+var lastName = "satpute";////Variable declarat and  Initialization
+firstName = "king Kohli"; //variable update
+
+var firstName; // variable re- declaration
+console.log(firstName);
+
+
