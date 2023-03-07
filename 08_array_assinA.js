d91a7f77114fbed4b515b71285a2f6d26daa5ed8
amrsatpute <amardev862@gmail.com>
2023-03-07 15:07:44 +0530

    Changes in Array
    Array

--- 08_array_assinA.js
+++ 08_array_assinA.js
@@ -1,11 +1,17 @@
 console.log(`---------- 1 -----------`);
-const arraySeasonalFruits = ["Banana", "Orange", "Apple", "Mango", "Water Melon"];
-console.log('First element of the array');
-let first = arraySeasonalFruits[0]
+const arraySeasonalFruits = [
+  "Banana",
+  "Orange",
+  "Apple",
+  "Mango",
+  "Water Melon",
+];
+console.log("First element of the array");
+let first = arraySeasonalFruits[0];
 console.log(first);
 
-console.log('Last element of the array');
-let last = arraySeasonalFruits[arraySeasonalFruits.length-1]
+console.log("Last element of the array");
+let last = arraySeasonalFruits[arraySeasonalFruits.length - 1];
 console.log(last);
 
 console.log(`---------- 2 -----------`);
@@ -15,7 +21,7 @@ console.log(arraySeasonalFruits);
 
 console.log(`---------- 3 -----------`);
 console.log(`Removing mongo from the array`);
-let remove = arraySeasonalFruits.splice(4,1)
+let remove = arraySeasonalFruits.splice(4, 1);
 console.log(remove);
 console.log(arraySeasonalFruits);
 
@@ -29,18 +35,17 @@ console.log(`Adding Lychee Fruit before Water Melon`);
 arraySeasonalFruits.splice(4, 0, "Lychee Fruit");
 console.log(arraySeasonalFruits);
 
-
 console.log(`---------- 6 -----------`);
 console.log(`Replacing Orange with Jujube`);
-arraySeasonalFruits.splice(2 , 1 ,"Jujube");
+arraySeasonalFruits.splice(2, 1, "Jujube");
 console.log(arraySeasonalFruits);
 
 console.log(`---------- 7 -----------`);
 console.log(`Log element from 1 to 4`);
-let elements = arraySeasonalFruits.splice(1 , 3);
+let elements = arraySeasonalFruits.splice(1, 3);
 console.log(elements);
 
 console.log(`---------- 8 -----------`);
 console.log(`Select last 3 elements`);
-const last3 =arraySeasonalFruits.slice(-3);
+const last3 = arraySeasonalFruits.slice(-3);
 console.log(last3);
